858b5d303474f12e57ff3846e2351aa1538b4916
Mike Fehrenbach <mfehrenb@opentext.com>
2021-02-04 14:32:08 -0500

    add in superuser functionality

--- app/users/models.py
+++ app/users/models.py
@@ -4,8 +4,18 @@ from django.db import models
 
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **kwargs):
+        if(not isinstance(email, str)):
+            raise ValueError('Email is invalid')
         user = self.model(email=self.normalize_email(email), **kwargs)
         user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password=None, **kwargs):
+        user = self.create_user(email, password, **kwargs)
+        user.is_superuser = True
+        user.is_staff = True
         user.save(using=self._db)
 
         return user
--- app/users/tests/tests_models.py
+++ app/users/tests/tests_models.py
@@ -5,18 +5,32 @@ from pytest_django.plugin import django_db_blocker
 
 
 class TestsModels:
+    mock_password = '123'
+    mock_email = 'test@opentext.com'
+
     @pytest.mark.django_db
     def test_success_create_user_with_email(self):
-        email = "test@opentext.com"
-        password = 'test_pass'
-        user = get_user_model().objects.create_user(email, password)
+        user = get_user_model().objects.create_user(self.mock_email, self.mock_password)
 
-        assert user.email == email
-        assert user.check_password(password) is True
+        assert user.email == self.mock_email
+        assert user.check_password(self.mock_password) is True
 
     @pytest.mark.django_db
     def test_success_normalize_email(self):
-        email = "test@OPENTEXT.COM"
-        user = get_user_model().objects.create_user(email, '123')
+        email = 'test@OPENTEXT.COM'
+        user = get_user_model().objects.create_user(email, self.mock_password)
 
         assert user.email == email.lower()
+
+    @pytest.mark.django_db
+    def test_error_email_is_none(self):
+        with pytest.raises(ValueError):
+            get_user_model().objects.create_user(None, self.mock_password)
+
+
+    @pytest.mark.django_db
+    def test_success_create_superuser(self):
+        user = get_user_model().objects.create_superuser(self.mock_email, self.mock_password)
+
+        assert user.is_superuser is True
+        assert user.is_staff is True
