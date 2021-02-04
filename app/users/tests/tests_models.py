0060939ffcd6e8382c5b28dd823ca812449a5dfc
Mike Fehrenbach <mfehrenb@opentext.com>
2021-02-04 12:59:17 -0500

    make sure emails are lower case

--- app/users/tests/tests_models.py
+++ app/users/tests/tests_models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
 
 import pytest
+from pytest_django.plugin import django_db_blocker
 
 
 class TestsModels:
@@ -8,7 +9,14 @@ class TestsModels:
     def test_success_create_user_with_email(self):
         email = "test@opentext.com"
         password = 'test_pass'
-        user = get_user_model().objects.create_user(email=email, password=password)
+        user = get_user_model().objects.create_user(email, password)
 
         assert user.email == email
         assert user.check_password(password) is True
+
+    @pytest.mark.django_db
+    def test_success_normalize_email(self):
+        email = "test@OPENTEXT.COM"
+        user = get_user_model().objects.create_user(email, '123')
+
+        assert user.email == email.lower()
